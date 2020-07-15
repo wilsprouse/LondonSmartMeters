fcc578f6edd6318e48074714c1f8601110e845f7
BuildTools <unconfigured@null.spigotmc.org>
2020-07-14 22:13:35 -0700

    Push via run.sh

--- smartmeters.py
+++ smartmeters.py
@@ -113,7 +113,7 @@ class SeasonalEnergyUse:
                     tempDayDict[row['day']] = row['energy_sum'], 1
 
         for day in tempDayDict.items():
-            self.avgDayDict[day[0]] = day[1]/day[2]
+            self.avgDayDict[day[0]] = day[1][0]/day[1][1]
 
         print(self.avgDayDict)
 
