1e047a62430230a6bfbae3ca1b6f05d71a4c396d
BuildTools <unconfigured@null.spigotmc.org>
2020-07-14 19:00:58 -0700

    First Commit

---
+++ smartmeters.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+#filePD = pd.read_csv("block_0.csv", usecols=["LCLid", "day"])
+
+
+#print(filePD)
+
+
+class MostData:
+    """This Class is used to produce a year that has the most data points
+       since we only want to evaluate a single year's worth of data. We can use
+       this class to produce a visual, and to have our program automatically
+       suggest what year to use.
+    """
+
+    def __init__(self):
+        """Initialize, we only need LCLid and the days in which data was collected"""
+        self.file_data = pd.read_csv("block_0.csv", usecols=["LCLid", "day"])
+        self.days = {}
+        self.years = {"2011": 0, "2012": 0, "2013": 0, "2014": 0 }
+
+    def getData(self):
+        """Returns the file data in column x row form"""
+        return self.file_data
+    
+    def dataByDay(self):
+        """This Method creates a dictionary of the days and how many data points
+           the days have
+        """
+
+        sortedByDate = self.getData().sort_values(by='day')
+
+        for (index, row) in sortedByDate.iterrows():
+            if row["day"] in self.days:
+                self.days[row["day"]] += 1
+            else:
+                self.days[row["day"]] = 1 
+        #print(days)
+
+    def dataByYear(self):
+        """Gets the Average amount of data points per day for each year"""
+
+        self.dataByDay()
+
+        for key in self.days:
+            self.years[key[:4]] += self.days[key]
+
+    def displayYear(self):
+        """Bar Graph of the average amount of data points per day by year"""
+        self.dataByYear()
+        xAxis = ['2011', '2012', '2013', '2014']
+        energyUseAverage = [self.years['2011']/365, self.years['2012']/365, self.years['2013']/365, self.years['2014']/365]
+
+
+        x_pos = [i for i, _ in enumerate(xAxis)]
+
+        plt.bar(x_pos, energyUseAverage, color='green')
+
+        plt.xlabel("Year")
+        plt.ylabel("Average Data Points Per Day")
+        plt.title("Energy Usage Data Points by Year from 2011-2014")
+
+        plt.xticks(x_pos, xAxis)
+
+        plt.show()
+
+
+    def displayDay(self):
+        self.DataByDay()
+        plt.plot(*zip(*sorted(self.days.items())),color="green")
+        plt.title("Average Household Data From 2012-2014")
+        plt.xlabel('Year')
+        plt.ylabel('Households')
+        plt.show()
+
+
+
+
+def driver():
+
+    most_data = MostData()
+
+    # most_data.displayDay() # Use this to see data per day from 2011-2014
+
+    most_data.displayYear() # Use this to see a bar graph of the best year
+
+
+driver()
